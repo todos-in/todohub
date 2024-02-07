1f3e064bbff44e62fe95bc8f373df8fff92ef7b9
nigeisel <nigeisel@users.noreply.github.com>
2024-02-07 18:57:33 +0100

    Add regex tests for multiple and negative results

--- __tests__/regex.test.ts
+++ __tests__/regex.test.ts
@@ -14,6 +14,22 @@ describe('Regex unit tests', () => {
     expect(matches[0]).toHaveProperty('todo_text', 'default match case');
   })
 
+  it('match multiple TODOs', async () => {
+    const matches = matchTodos(
+      `// TODO (#1) fix me
+      function sum(a, b) {
+        return a-b;  // TODO (#2) here could be the problem
+      }`
+    )
+    expect(matches).toHaveLength(2);
+    expect(matches[0]).toHaveProperty('todo_keyword', 'TODO');
+    expect(matches[0]).toHaveProperty('issue_number', '1');
+    expect(matches[0]).toHaveProperty('todo_text', 'fix me');
+    expect(matches[1]).toHaveProperty('todo_keyword', 'TODO');
+    expect(matches[1]).toHaveProperty('issue_number', '2');
+    expect(matches[1]).toHaveProperty('todo_text', 'here could be the problem');
+  })
+
   it('keyword case variation 1', async () => {
     const matches = matchTodos(`// todo (#1823) keyword case variation 1`)
     expect(matches).toHaveLength(1);
@@ -92,5 +108,26 @@ describe('Regex unit tests', () => {
     expect(matches[0]).toHaveProperty('todo_keyword', 'TODO');
     expect(matches[0]).toHaveProperty('issue_number', '1823');
     expect(matches[0]).toHaveProperty('todo_text', 'closing parenthesis is missing');
+  })
+
+  it('negative: typo in TODO', async () => {
+    const matches = matchTodos(`// TODU (#1823) typo in TODO`)
+    expect(matches).toHaveLength(0);
+  })
+
+  it('negative: typo in TODO', async () => {
+    const matches = matchTodos(`// TODU (#1823) typo in TODO`)
+    expect(matches).toHaveLength(0);
+  })
+
+  it('negative: text in issuenumber', async () => {
+    const matches = matchTodos(`// TODO (#text) text in issuenumber`)
+    expect(matches).toHaveLength(0);
+  })
+
+  it('negative: line break should break matching', async () => {
+    const matches = matchTodos(`// TODO
+    (#1823) line break should break matching`)
+    expect(matches).toHaveLength(0);
   })
 })
