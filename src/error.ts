6a193e292cc61c3b50183facdb26371402bc6d6e
nigeisel <nigeisel@users.noreply.github.com>
2024-03-11 17:44:14 +0100

    add application specific errors

--- src/error.ts
+++ src/error.ts
@@ -1,1 +1,77 @@
-export class EnvironmentLoadError extends Error {}
+import assert from 'node:assert'
+import { RequestError } from '@octokit/request-error'
+
+export class TodohubError extends Error {
+  code: string
+  cause?: Error
+  debugInformation?: object
+
+  /**
+   * @param message Message displayed to users (in non-debug environment)
+   * @param code Application specific code that should not change from occurrence to occurrence (Displayed to users)
+   * @param debugInformation Generic object with additional information meant to be displayed in debug logs only
+   * @param cause Original Error that caused the Exception
+   */
+  constructor(message: string, code?: string, debugInformation?: object, cause?: Error) {
+    super(message)
+    this.code = code || this.constructor.name
+    this.cause = cause
+    this.debugInformation = debugInformation
+  }
+
+  log() {
+    return `${this.code}: ${this.message}`
+  }
+
+  debugLog() {
+    return JSON.stringify({
+      message: this.message,
+      code: this.code,
+      stack: this.stack,
+      debugInformation: this.debugInformation,
+      cause: {
+        message: this.cause?.message,
+        stack: this.cause?.stack,
+        errorName: this.cause?.constructor.name,
+      },
+    })
+  }
+}
+
+export class ApiError extends TodohubError {
+  constructor(message: string, debugInfo?: object) {
+    super(message, 'api-error', debugInfo)
+  }
+}
+
+export class EnvironmentLoadError extends TodohubError {
+  constructor(debugInformation: { place: string, key: string }) {
+    super(`Failed to load from environment: ${debugInformation.key}`, 'env-load', debugInformation)
+  }
+}
+
+export class EnvironmentParsingError extends TodohubError {
+  constructor(message: string) { super(message, 'env-parse') }
+}
+
+export class IssueNotInStateError extends TodohubError {
+  constructor(issueNr: number) {
+    super(`Issue number ${issueNr} not found in Todohub control issues state.`, 'issue-not-in-state', { issueNr: issueNr })
+  }
+}
+
+export class ControlIssueParsingError extends TodohubError {
+  constructor(message: string) { super(message, 'control-issue-parse') }
+}
+
+export function assersTodohubError(error: unknown): asserts error is TodohubError {
+  assert(error instanceof TodohubError)
+}
+
+export function assertError(error: unknown): asserts error is Error {
+  assert(error instanceof Error)
+}
+
+export function assertGithubError(error: unknown): asserts error is RequestError {
+  assert(error instanceof RequestError)
+}
