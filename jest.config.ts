de2b73b3e47c089439efa05d07871b43432d7c40
nigeisel <nigeisel@users.noreply.github.com>
2024-03-13 22:17:00 +0100

    update typescript config * enable typescript checking in test folder, but have a separate ts config for building (which doesnt add tests to /dist)

--- jest.config.ts
+++ jest.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
     '^.+\\.ts$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.test.json',
+        tsconfig: 'tsconfig.json',
         useESM: true,
       },
     ],
