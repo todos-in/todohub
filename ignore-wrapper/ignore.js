b1edec8c47b6b72bd8a19fb51495d910237f2201
nigeisel <nigeisel@users.noreply.github.com>
2024-04-19 17:16:25 +0200

    rename main -> runner

--- ignore-wrapper/ignore.js
+++ ignore-wrapper/ignore.js
@@ -1,3 +1,5 @@
+// Note: necessary because ignore package can't be imported properly as esm at the moment: https://github.com/kaelzhang/node-ignore/issues/96
+
 const ignore = require('ignore')
 
 module.exports = {ignoreWrapper: ignore.default}
