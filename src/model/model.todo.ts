b48bbdfad482a0bf8ba336ecd84b266ecf63c905
nigeisel <nigeisel@users.noreply.github.com>
2026-04-05 18:31:35 +0200

    sort Todos before writing to datastore

--- src/model/model.todo.ts
+++ src/model/model.todo.ts
@@ -19,4 +19,31 @@ export class Todo {
     Object.assign(this, todo)
   }
 
+  compare(otherTodo: Todo) {
+    if (this.issueNumber !== otherTodo.issueNumber) {
+      if (this.issueNumber == null) return 1
+      if (otherTodo.issueNumber == null) return -1
+      return this.issueNumber - otherTodo.issueNumber
+    }
+
+    if (this.fileName !== otherTodo.fileName) {
+      return this.fileName.localeCompare(otherTodo.fileName)
+    }
+
+    if (this.lineNumber !== otherTodo.lineNumber) {
+      return this.lineNumber - otherTodo.lineNumber
+    }
+
+    if (this.todoText !== otherTodo.todoText) {
+      return this.todoText.localeCompare(otherTodo.todoText)
+    }
+
+    if (this.foundInCommit !== otherTodo.foundInCommit) {
+      if (this.foundInCommit == null) return 1
+      if (otherTodo.foundInCommit == null) return -1
+      return this.foundInCommit.localeCompare(otherTodo.foundInCommit)
+    }
+
+    return 0
+  }
 }
